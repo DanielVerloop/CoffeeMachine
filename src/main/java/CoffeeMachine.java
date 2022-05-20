718cff96fdc73706e7c1855c3baa417f46a8bc99
DanielVerloop <a.c.verloop@student.tue.nl>
2022-05-20 09:54:17 +0200

    init project

---
+++ src/main/java/CoffeeMachine.java
@@ -0,0 +1,37 @@
+public class CoffeeMachine {
+    private int coffeeBeans;
+    private int milk;
+
+    public CoffeeMachine() {
+
+    }
+
+    public CoffeeMachine(int coffeeBeans, int milk) {
+        this.coffeeBeans = coffeeBeans;
+        this.milk = milk;
+    }
+
+    public int getCoffeeBeans() {
+        return coffeeBeans;
+    }
+
+    public void setCoffeeBeans(int coffeeBeans) {
+        this.coffeeBeans = coffeeBeans;
+    }
+
+    public int getMilk() {
+        return milk;
+    }
+
+    public void setMilk(int milk) {
+        this.milk = milk;
+    }
+
+    public void makeCoffee() {
+        if (getCoffeeBeans() > 0) {
+            this.coffeeBeans--;
+        } else {
+            throw new IllegalStateException("No coffee beans left!");
+        }
+    }
+}
